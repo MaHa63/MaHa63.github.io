78945f9f0b02d91222934923ed0edb856a5ebdad
Matti Harsu <matti.harsu@gmail.com>
2018-03-08 16:38:34 +0200

    HTTP GET kysely ilman tietokantaa

--- src/server.js
+++ src/server.js
@@ -4,14 +4,30 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/matti', function(req, res) {
+app.get('/list', function(request, response) {
 	console.log('Serverissä, Matti');
-	return res.send(JSON.stringify('tulossa'));
+
+	let data = [ 
+				{"id"     : "1",
+	              "name"  : "Clean room",
+	              "owner" : "Matti"
+	              },
+	             
+	              {
+	               "id"    : "2",
+	               "name"  : "Wash car",
+	               "owner" : "Marko"
+	              }
+	           ];
+
+
+	return response.send(data);
 });
 
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 
 app.listen(process.env.PORT || 8080);
